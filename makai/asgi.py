56652c4c229382cdf693bd42b935326cdfa1b50a
jerry syre <jeremyoryono@gmail.com>
2026-05-07 16:00:48 +0300

    new

--- makai/asgi.py
+++ makai/asgi.py
@@ -1,11 +1,14 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'makai.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from chats.consumers import ChatConsumer
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'makai.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
